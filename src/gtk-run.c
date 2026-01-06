49ad7c6dc489884a1ad7e74d67f3785fa87587cd
Simon Long <simon@raspberrypi.com>
2026-01-06 07:17:51 +0000

    Better name

--- src/gtk-run.c
+++ src/gtk-run.c
@@ -329,7 +329,7 @@ int main (int argc, char *argv[])
 
     gtk_init (&argc, &argv);
 
-    builder = gtk_builder_new_from_file (PACKAGE_UI_DIR "/gui-runner.ui");
+    builder = gtk_builder_new_from_file (PACKAGE_UI_DIR "/gui-runcmd.ui");
 
     win = (GtkWidget *) gtk_builder_get_object (builder, "main_wd");
     entry = (GtkWidget *) gtk_builder_get_object (builder, "entry_cmd");
