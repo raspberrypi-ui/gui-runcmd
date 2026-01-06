97cd03e22ebc1af64396b585a58ee5429ddb9046
Simon Long <simon@raspberrypi.com>
2026-01-06 13:21:06 +0000

    Tidy match app function

--- src/gtk-run.c
+++ src/gtk-run.c
@@ -70,6 +70,16 @@ static gboolean thread_term = FALSE;
 /* Prototypes                                                                 */
 /*----------------------------------------------------------------------------*/
 
+static void launch_application (const char *appname);
+static gpointer find_apps (gpointer user_data);
+static gboolean find_apps_done (gpointer user_data);
+static void reload_apps (MenuCache* cache, gpointer user_data);
+static void on_entry_changed (GtkEntry* entry, gpointer user_data);
+static MenuCacheApp* match_app_by_exec (const char* exec);
+static gboolean delete_event (GtkWidget *widget, GdkEvent *event, gpointer user_data);
+static void button_handler (GtkWidget *widget, gpointer user_data);
+static gboolean key_press_event (GtkWidget *widget, GdkEventKey *event, gpointer user_data);
+
 /*----------------------------------------------------------------------------*/
 /* Function definitions                                                       */
 /*----------------------------------------------------------------------------*/
@@ -78,110 +88,6 @@ static void launch_application (const char *appname)
 {
     char *cmd[2] = {(char *) appname, NULL};
     g_spawn_async (NULL, cmd, NULL, G_SPAWN_SEARCH_PATH, NULL, NULL, NULL, NULL);
-}
-
-static MenuCacheApp* match_app_by_exec(const char* exec)
-{
-    GSList* l;
-    MenuCacheApp* ret = NULL;
-    char* exec_path = g_find_program_in_path(exec);
-    const char* pexec;
-    int path_len, exec_len, len;
-
-    if( ! exec_path )
-        return NULL;
-
-    path_len = strlen(exec_path);
-    exec_len = strlen(exec);
-
-    for( l = app_list; l; l = l->next )
-    {
-        MenuCacheApp* app = MENU_CACHE_APP(l->data);
-        const char* app_exec = menu_cache_app_get_exec(app);
-        if ( ! app_exec)
-            continue;
-
-        if( g_path_is_absolute(app_exec) )
-        {
-            pexec = exec_path;
-            len = path_len;
-        }
-        else
-        {
-            pexec = exec;
-            len = exec_len;
-        }
-
-        if( strncmp(app_exec, pexec, len) == 0 )
-        {
-            /* exact match has the highest priority */
-            if( app_exec[len] == '\0' )
-            {
-                ret = app;
-                break;
-            }
-            /* those matches the pattern: exe_name %F|%f|%U|%u have higher priority */
-            if( app_exec[len] == ' ' )
-            {
-                if( app_exec[len + 1] == '%' )
-                {
-                    if( strchr( "FfUu", app_exec[len + 2] ) )
-                    {
-                        ret = app;
-                        break;
-                    }
-                }
-                ret = app;
-            }
-        }
-    }
-
-    /* if this is a symlink */
-    if( ! ret && g_file_test(exec_path, G_FILE_TEST_IS_SYMLINK) )
-    {
-        char target[512]; /* FIXME: is this enough? */
-        len = readlink( exec_path, target, sizeof(target) - 1);
-        if( len > 0 )
-        {
-            target[len] = '\0';
-            ret = match_app_by_exec(target);
-            if( ! ret )
-            {
-                /* FIXME: Actually, target could be relative paths.
-                 *        So, actually path resolution is needed here. */
-                char* basename = g_path_get_basename(target);
-                char* locate = g_find_program_in_path(basename);
-                if( locate && strcmp(locate, target) == 0 )
-                {
-                    ret = match_app_by_exec(basename);
-                    g_free(locate);
-                }
-                g_free(basename);
-            }
-        }
-    }
-
-    g_free(exec_path);
-    return ret;
-}
-
-static gboolean find_apps_done (gpointer user_data)
-{
-    if (thread_term) return FALSE;
-
-    GtkEntryCompletion* comp = gtk_entry_completion_new ();
-    gtk_entry_completion_set_minimum_key_length (comp, 1);
-    gtk_entry_completion_set_inline_completion (comp, TRUE);
-    gtk_entry_completion_set_popup_set_width (comp, TRUE);
-    gtk_entry_completion_set_popup_single_match (comp, FALSE);
-    gtk_entry_completion_set_model (comp, GTK_TREE_MODEL (path_apps));
-    gtk_entry_completion_set_text_column (comp, 0);
-    gtk_entry_set_completion (GTK_ENTRY (entry), comp);
-    gtk_entry_completion_complete (comp);
-    g_object_unref (comp);
-    g_object_unref (path_apps);
-
-    return FALSE;
 }
 
 static gpointer find_apps (gpointer user_data)
@@ -227,17 +133,30 @@ static gpointer find_apps (gpointer user_data)
     return NULL;
 }
 
-static void mc_unref (gpointer data, gpointer user_data)
+static gboolean find_apps_done (gpointer user_data)
 {
-    MenuCacheItem *item = (MenuCacheItem *) data;
-    menu_cache_item_unref (item);
+    if (thread_term) return FALSE;
+
+    GtkEntryCompletion* comp = gtk_entry_completion_new ();
+    gtk_entry_completion_set_minimum_key_length (comp, 1);
+    gtk_entry_completion_set_inline_completion (comp, TRUE);
+    gtk_entry_completion_set_popup_set_width (comp, TRUE);
+    gtk_entry_completion_set_popup_single_match (comp, FALSE);
+    gtk_entry_completion_set_model (comp, GTK_TREE_MODEL (path_apps));
+    gtk_entry_completion_set_text_column (comp, 0);
+    gtk_entry_set_completion (GTK_ENTRY (entry), comp);
+    gtk_entry_completion_complete (comp);
+    g_object_unref (comp);
+    g_object_unref (path_apps);
+
+    return FALSE;
 }
 
 static void reload_apps (MenuCache* cache, gpointer user_data)
 {
     if (app_list)
     {
-        g_slist_foreach (app_list, mc_unref, NULL);
+        g_slist_foreach (app_list, (GFunc) menu_cache_item_unref, NULL);
         g_slist_free (app_list);
     }
     app_list = menu_cache_list_all_apps (cache);
@@ -260,6 +179,74 @@ static void on_entry_changed (GtkEntry* entry, gpointer user_data)
         }
     }
     gtk_image_set_from_icon_name (GTK_IMAGE (icon), "gtk-execute", GTK_ICON_SIZE_DND);
+}
+
+static MenuCacheApp *match_app_by_exec (const char* exec)
+{
+    GSList *l;
+    MenuCacheApp *app, *ret = NULL;
+    char *exec_path;
+    const char *pexec, *app_exec;
+    int len;
+
+    exec_path = g_find_program_in_path (exec);
+    if (!exec_path) return NULL;
+
+    for (l = app_list; l; l = l->next)
+    {
+        app = MENU_CACHE_APP (l->data);
+        app_exec = menu_cache_app_get_exec (app);
+        if (!app_exec) continue;
+
+        if (g_path_is_absolute (app_exec)) pexec = exec_path;
+        else pexec = exec;
+        len = strlen (pexec);
+
+        if (strncmp (app_exec, pexec, len) == 0)
+        {
+            /* exact match has the highest priority */
+            if (app_exec[len] == 0)
+            {
+                ret = app;
+                break;
+            }
+
+            /* those matches the pattern: exe_name %F|%f|%U|%u have higher priority */
+            if (app_exec[len] == ' ')
+            {
+                ret = app;
+                if (app_exec[len + 1] == '%' && strchr ("FfUu", app_exec[len + 2])) break;
+            }
+        }
+    }
+
+    /* if this is a symlink */
+    if (!ret && g_file_test (exec_path, G_FILE_TEST_IS_SYMLINK))
+    {
+        char target[512]; /* FIXME: is this enough? */
+        len = readlink (exec_path, target, sizeof (target) - 1);
+        if (len > 0)
+        {
+            target[len] = '\0';
+            ret = match_app_by_exec (target);
+            if (!ret)
+            {
+                /* FIXME: Actually, target could be relative paths.
+                 *        So, actually path resolution is needed here. */
+                char *basename = g_path_get_basename (target);
+                char *locate = g_find_program_in_path (basename);
+                if (locate && strcmp (locate, target) == 0)
+                {
+                    ret = match_app_by_exec (basename);
+                    g_free (locate);
+                }
+                g_free (basename);
+            }
+        }
+    }
+
+    g_free (exec_path);
+    return ret;
 }
 
 /* UI handlers */
@@ -341,7 +328,7 @@ int main (int argc, char *argv[])
     /* free app list */
     if (app_list)
     {
-        g_slist_foreach (app_list, mc_unref, NULL);
+        g_slist_foreach (app_list, (GFunc) menu_cache_item_unref, NULL);
         g_slist_free (app_list);
     }
 
